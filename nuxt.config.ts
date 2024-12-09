80a3de35fc400bfe191b40f3369d3e1e318089b1
HanyMohamed <hsaad9231@gmail.com>
2024-12-09 11:58:32 +0200

    ✨feat: add Header Section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   app: {
     head: {
       script: [
@@ -14,8 +15,15 @@ export default defineNuxtConfig({
           src: "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js",
         },
       ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic",
+        },
+      ],
     },
   },
+
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
